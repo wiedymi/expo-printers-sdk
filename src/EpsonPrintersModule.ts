2e3e2cb194b49bb1163646368fa7023d49986e24
atrafimchyk <duben547@gmail.com>
2025-04-03 12:21:37 +0200

    update RN definitions for Eposon.

--- src/EpsonPrintersModule.ts
+++ src/EpsonPrintersModule.ts
@@ -1,18 +1,10 @@
 import { NativeModule, requireNativeModule } from "expo";
-
-import type {
-  EpsonPrintersModuleEvents,
-  EpsonPrintOptions,
-  EpsonPrinterStatus,
-} from "./EpsonPrinters.types";
+import type { EpsonPrintersModuleEvents, EpsonPrinterInfo } from "./EpsonPrinters.types";
+import type { PrinterConnectionType } from "./commons";
 
 declare class EpsonPrintersModule extends NativeModule<EpsonPrintersModuleEvents> {
-  startDiscovery(type: number): Promise<boolean>;
-  stopDiscovery(): Promise<boolean>;
-  printImage(base64Image: string, options: EpsonPrintOptions): Promise<boolean>;
-  getPrinterStatus(): Promise<EpsonPrinterStatus>;
-  connectPrinter(ip: string): Promise<boolean>;
-  disconnectPrinter(): Promise<boolean>;
+  findPrinters(connectionType: PrinterConnectionType): Promise<boolean>;
+  printImage(base64Image: string, deviceData: EpsonPrinterInfo): Promise<boolean>;
 }
 
 // This call loads the native module object from the JSI.
